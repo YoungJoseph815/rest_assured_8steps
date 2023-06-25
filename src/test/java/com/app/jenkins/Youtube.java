d5b11b60b20c3c559807e4d36a0fb60308db1128
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-06-25 10:57:09 -0400

    jenkins
    watch these videos

---
+++ src/test/java/com/app/jenkins/Youtube.java
@@ -0,0 +1,12 @@
+package com.app.jenkins;
+
+public class Youtube {
+
+    //TODO Watch these videos in this order
+
+    //https://www.youtube.com/watch?v=5Jvd6p9GUP4
+
+    //https://www.youtube.com/watch?v=-4iMrktR4ZQ&t=648s
+
+    //https://www.youtube.com/watch?v=MNzNPCJJqaI
+}
